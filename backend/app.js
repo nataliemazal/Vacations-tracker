e9fc62d5e7e641d8beb5e3832383f6b4e369c149
nataliemazal <77893840+nataliemazal@users.noreply.github.com>
2021-09-09 22:22:36 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,18 +15,12 @@ const socketHelper = require("./helpers/socket-helper")
 server.use(fileUpload());
 server.use(cors());
 server.use(express.json());
-//for hosting
-server.use(express.static(path.join(__dirname,"/frontend")))
-//*
+
 server.use("/api", userController);
 server.use("/api/admin", adminController);
 server.use("/api/auth", authController);
 
-//*
-server.use("*",(request,response)=>{
-    response.sendFile(path.join(__dirname,"/frontend/public/index.html"))
-})
-//*
+
 
 const listener = server.listen(PORT, () => console.log(`Lisiting on port ${PORT}...`));
 
